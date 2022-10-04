caf183020de9bc96aba15b722a936e60e61389a8
Федоров Евгений <zen-g@mail.ru>
2022-10-04 14:42:56 +0300

    Solved the first task

--- HomeWorks/HomeWork2/Program.cs
+++ HomeWorks/HomeWork2/Program.cs
@@ -1,5 +1,24 @@
 ﻿
 
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+Console.WriteLine("Enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-
+if (number > 100 && number < 999)
+{
+    int n1 = number % 100;
+    int n2 = n1 / 10;
+    Console.WriteLine(n2);
+}
+else if (number < -100 && number > -999)
+{
+    int a1 = number % 100;
+    int a2 = a1 / 10;
+    int a3 = a2 * -1;
+    Console.WriteLine(a3);
+}
+else
+{
+    Console.WriteLine("It's not a three digit number");
+}
