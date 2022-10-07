8d73cbe7155039effd1be1af75e2140dbedf0e70
Федоров Евгений <zen-g@mail.ru>
2022-10-07 16:41:23 +0300

    Solved the first task

---
+++ HomeWorks/HomeWork3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Написал программу для двузначных чисел и выше 
+  
+Console.WriteLine("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = number;
+int number1 = number % 10;
+
+if (number < 10)
+{
+    Console.WriteLine("The number is incorrect");
+}
+else
+{
+    while (number > 10)
+    {
+        number = number / 10;
+    }
+
+    if (number == number1)
+    {
+        Console.WriteLine($"Yes, the number {a} is a palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"No, the number {a} is nota palindrome");
+    }
+}
+
+
