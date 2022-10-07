ddd77c79f184d4251abaff796d62b590b262a39c
Федоров Евгений <zen-g@mail.ru>
2022-10-07 17:09:18 +0300

    Solved the second task

--- HomeWorks/HomeWork3/Program.cs
+++ HomeWorks/HomeWork3/Program.cs
@@ -1,8 +1,8 @@
-﻿
+﻿/*
 
 // Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // Написал программу для двузначных чисел и выше 
-  
+
 Console.WriteLine("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -30,4 +30,30 @@ else
     }
 }
 
+*/
+
+
+
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+void Distance(double a1, double b1, double c1, double a2, double b2, double c2)
+{
+    double d = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2) + Math.Pow(c2 - c1, 2));
+    Console.WriteLine(Math.Round(d, 2));
+}
+
+Console.WriteLine("Input xA - coordinate");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input yA - coordinate");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input cA - coordinate");
+double c1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input xB - coordinate");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input yB - coordinate");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input cB - coordinate");
+double c2 = Convert.ToDouble(Console.ReadLine());
+
+Distance(x1, y1, c1, x2, y2, c2);
 
