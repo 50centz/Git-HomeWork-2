dcda9fd9d1aa626c2e27eeebe334ed8f56b53173
Федоров Евгений <zen-g@mail.ru>
2022-10-12 12:10:44 +0300

    Solved the second task

--- HomeWorks/HomeWork4/Program.cs
+++ HomeWorks/HomeWork4/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
@@ -27,3 +27,35 @@ Console.WriteLine("Enter the degree: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 RaiseToADegree(a, b);
+
+*/
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Sum(int number)
+{
+    int a = 0;
+    int b = 0;
+    if (number > 0)
+    {
+        while (number > 0)
+        {
+            a = number % 10;
+            b = b + a;
+            number = number / 10;
+        }
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine("The number is incorrect !");
+    }
+}
+
+Console.WriteLine("Input a number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Sum(b);
