febc377b8728e9458a1af9ad472a5e5b11d9e6c8
Федоров Евгений <zen-g@mail.ru>
2022-10-14 12:53:23 +0300

    Solved the first task

---
+++ HomeWorks/HomeWork5/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int NumberOfEvenNumbersInTheArray(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) sum++;
+    }
+    return sum;
+}
+
+Console.WriteLine("Input a size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(NumberOfEvenNumbersInTheArray(array));
