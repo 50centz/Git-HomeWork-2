f196fde002ed04807cfae0f84712300b599f0471
Федоров Евгений <zen-g@mail.ru>
2022-10-21 13:52:50 +0300

    Solved the second task

--- HomeWorks/HomeWork7/Program.cs
+++ HomeWorks/HomeWork7/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
 double[,] CreateRandom2dArray()
@@ -41,4 +41,27 @@ void Show2dArray(double[,] array)
 
 double[,] newArray = CreateRandom2dArray();
 Show2dArray(newArray);
+*/
 
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+void ThereIsNoSuchElement()
+{
+    Console.WriteLine("There is no such element");
+}
+
+void ReturnTheValueOfTheElement(int[,] array, int a, int b)
+{
+    if (a <= array.GetLength(0) && b <= array.GetLength(1))
+    {
+        Console.WriteLine(array[a - 1, b - 1]);
+    }
+    else
+    {
+        ThereIsNoSuchElement();
+    }
+
+}
