c6a54cadafc8bd579a2aee6cd1013c8a091e4995
Федоров Евгений <zen-g@mail.ru>
2022-11-08 16:55:15 +0300

    Solved second task

---
+++ HomeWorks/HomeWork8/Program.cs
@@ -0,0 +1,75 @@
+﻿int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int FindRow(int[,] array)
+{
+    int sum = 0;
+    int result = 0;
+    int numberString = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        result += array[0, j];
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (result > sum)
+        {
+            result = sum;
+            numberString = i;
+        }
+        sum = 0;
+    }
+    return numberString + 1;
+
+}
+
+int[,] myArray = CreateRandom2dArray();
+int numberRow = FindRow(myArray);
+Console.WriteLine();
+Console.WriteLine($"The smallest sum of elements in {numberRow} lines");
+
+
+
+
