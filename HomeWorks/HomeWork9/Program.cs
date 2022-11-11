3174da7b75c67586405d740cd4f4a34419d148fe
Федоров Евгений <zen-g@mail.ru>
2022-11-11 13:47:43 +0300

    8 seminar completed

---
+++ HomeWorks/HomeWork9/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void NaturalNumbersInTheInterval (int count)
+{
+    Console.Write(count + " ");
+    if(count > 1)
+    {
+        count--;
+        NaturalNumbersInTheInterval(count);
+    }
+}
+
+Console.WriteLine("Input a number: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+NaturalNumbersInTheInterval(digit);
+*/
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int TheSumOfNaturalElements(int m, int n)
+{
+    int sum = 0;
+
+    Console.Write($"{m} and {n}  ");
+    if (m < n)
+    {
+        m++;
+        TheSumOfNaturalElements(m, n);
+    }
+    sum += m;
+    return sum;
+}
+
+int a = 1;
+int b = 15;
+
+int sum = TheSumOfNaturalElements(a, b);
+Console.WriteLine();
+Console.WriteLine(sum);
---
+++ Seminars/SEMINAR9/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+void ShowNums(int n)
+{
+    if(n > 1) ShowNums(n - 1);
+
+    Console.Write(n + " ");
+}
+
+ShowNums(5);
+*/
+
+/*
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SumOfdigits(int num)
+{
+    if(num != 0) return SumOfdigits(num / 10) + num % 10;
+    else return 0;
+}
+
+Console.WriteLine(SumOfdigits(1234));
+*/
+
+/*
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void PrintNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        PrintNumbers(m, n - 1);
+        Console.Write($"{n} ");
+    }
+    if (m > n)
+    {
+        PrintNumbers(m - 1, n );
+        Console.Write($"{m} ");
+    }
+    if (m == n)
+    {
+        Console.Write(m + " ");
+    }
+}
+
+PrintNumbers(6, 2);
+*/
+
+
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+double PowerAB( double a, double b)
+{
+    if(b < 0) return (1/a) * PowerAB(a, b + 1);  // a ^ 
+    if(b > 0) return a * PowerAB(a, b - 1);
+    else return 1;
+}
+
+Console.WriteLine(PowerAB(2, 3));
+
+
