9c7653c0062b8b9a4e1a3c4c40716f4f54127ccf
Федоров Евгений <zen-g@mail.ru>
2022-11-11 13:55:40 +0300

    Solved first task

--- HomeWorks/HomeWork9/Program.cs
+++ HomeWorks/HomeWork9/Program.cs
@@ -1,22 +1,16 @@
 ﻿/*
 // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-void NaturalNumbersInTheInterval (int count)
+void NaturalNumbersInTheInterval(int a)
 {
-    Console.Write(count + " ");
-    if(count > 1)
-    {
-        count--;
-        NaturalNumbersInTheInterval(count);
-    }
+    Console.Write(a + " ");
+    if (a > 1) NaturalNumbersInTheInterval(a - 1);
 }
 
-Console.WriteLine("Input a number: ");
-int digit = Convert.ToInt32(Console.ReadLine());
-NaturalNumbersInTheInterval(digit);
+NaturalNumbersInTheInterval(10);
 */
 
-
+/*
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 int TheSumOfNaturalElements(int m, int n)
@@ -39,3 +33,7 @@ int b = 15;
 int sum = TheSumOfNaturalElements(a, b);
 Console.WriteLine();
 Console.WriteLine(sum);
+*/
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
